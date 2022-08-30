196932c31ed9d3039ba0094f1726cac8bd6c4a9f
SKhn-dev <78141923+SKhn-dev@users.noreply.github.com>
2022-08-30 12:12:24 +0100

    Update GitPractice.java

--- src/GitPractice.java
+++ src/GitPractice.java
@@ -1,7 +1,8 @@
 public class GitPractice {
     public static void main(String[] args) {
 
-        System.out.println("Hi guys lets change it");
+        //System.out.println("Hi guys lets change it");
         System.out.println("Malik's changes");
+        System.out.println("Shafaq's changes again");
     }
 }
