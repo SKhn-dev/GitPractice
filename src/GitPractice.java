61166f8bc10c7c9e290d4ea53eb7d7dfebf452e5
malik-111 <abdul.899@hotmail.com>
2022-08-30 12:10:35 +0100

    malik's changes

--- src/GitPractice.java
+++ src/GitPractice.java
@@ -1,5 +1,7 @@
 public class GitPractice {
     public static void main(String[] args) {
+
         System.out.println("Hi guys lets change it");
+        System.out.println("Malik's changes");
     }
 }
